12848f5126372d1fa1965460ccad8e1110268cd1
Falytha <brieztleice@gmail.com>
2025-05-14 14:44:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.image("IMG_0942.jpeg")
+st.image ("https://github.com/Falytha/falytha-tariwow/blob/main/IMG_6916.jpeg")
