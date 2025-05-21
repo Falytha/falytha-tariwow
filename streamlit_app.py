d2ba1b43335df11b5a1cde06e13c421333b0451d
Falytha <brieztleice@gmail.com>
2025-05-21 14:47:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,18 @@ st.subheader("lestari(kan bumi)")
 st.write("Mari bersatu menjadi avenger")
 st.write(
     """
-    - capt. america
-    - iron man
-    - sapiderman
+    - captain ameridut >in pict
+    - iron mancila >in pict
+    - sapiderbalabalaba >anomali
+    - wonder wokakamari >anomali
+    - badboboiboy
     """
 )
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
